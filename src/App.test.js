a9436579cbc4dc0840608d2d2b91db56a6b6fb5a
bram <david.j.bramwell@googlemail.com>
2018-03-24 14:31:49 +1300

    Fixed enzyme configuration and added test for navbar

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('contains Navbar when created', () => {
+  const app = mount(<App />);
+  const navbar = app.find('Navbar');
+  expect(navbar.length).toEqual(1);
 });
