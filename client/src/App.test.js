558f1ee4da37a9777f8a99e5e324fd8153dc0e77
bram <david.j.bramwell@googlemail.com>
2018-05-30 17:42:48 +0100

    Fix tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,26 +1,31 @@
-/* global it, expect */
+/* global it, expect, beforeEach */
 import React from 'react'
 import App from './App'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
+
+const initialState = {}
+const mockStore = configureStore()
+let store
+
+beforeEach(() => {
+  store = mockStore(initialState)
+})
 
 it('contains Menu when created', () => {
-  const app = shallow(<App />)
+  const app = shallow(<App store={store} />).dive()
   const menu = app.find('Menu')
   expect(menu.length).toBe(1)
-  expect(menu.props().navItems.length).toBe(3)
-  expect(menu.props().navItems[0]).toBe('The Russian Thrillers')
-  expect(menu.props().navItems[1]).toBe('The Black Eagle Trilogy')
-  expect(menu.props().navItems[2]).toBe('The Russian Tales')
 })
 
 it('contains a ScrollableAnchor with id "the-russian-thrillers"', () => {
-  const app = shallow(<App />)
+  const app = shallow(<App store={store} />).dive()
   const thrillersAnchor = app.find('ScrollableAnchor').at(1)
   expect(thrillersAnchor.props().id).toBe('the-russian-thrillers')
 })
 
 it('contains a ScrollableAnchor with id "home" and contains Home element', () => {
-  const app = shallow(<App />)
+  const app = shallow(<App store={store} />).dive()
   const homeAnchor = app.find('ScrollableAnchor').first()
   expect(homeAnchor.props().id).toBe('home')
   const home = homeAnchor.find('Home')
