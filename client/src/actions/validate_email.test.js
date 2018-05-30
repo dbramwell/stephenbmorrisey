0f7192d1e0736b444e3d71da4404e4fc7ef2853c
bram <david.j.bramwell@googlemail.com>
2018-05-30 17:11:19 +0100

    add dependency and fix typo

--- client/src/actions/validate_email.test.js
+++ client/src/actions/validate_email.test.js
@@ -5,7 +5,7 @@ describe('validateEmail', () => {
   it('should create an action to validate email in post request', () => {
     const uid = '123456789'
     const expectedAction = {
-      type: 'WALIDATE_EMAIL',
+      type: 'VALIDATE_EMAIL',
       payload: uid
     }
     expect(validateEmail(uid)).toEqual(expectedAction)
